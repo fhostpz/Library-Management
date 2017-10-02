e6972bd86d911d29dcb99db987cbda8da67a6eb7
LeowWeiJun <31387034+LeowWeiJun@users.noreply.github.com>
2017-10-03 00:52:49 +0800

    Add files via upload

---
+++ MaterialMainPage.java
@@ -0,0 +1,206 @@
+import javax.print.DocFlavor;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class MaterialMainPage {
+    private JButton addMaterialButton;
+    private JPanel materialMainPagePanel;
+    private JButton removeMaterialButton;
+    private JTable materialTable;
+    private JScrollPane tableScroll;
+    private JButton backButton;
+    private JButton editMaterialButton;
+
+    public JTable getMaterialTable() {
+        return materialTable;
+    }
+
+    public void setMaterialTable(JTable materialTable) {
+        this.materialTable = materialTable;
+    }
+
+    public JPanel getMaterialMainPagePanel() {
+        return materialMainPagePanel;
+    }
+
+    public MaterialMainPage(final JPanel panel) {
+        Vector<Vector<String>> data = new Vector<Vector<String>>(10);
+        String input;
+        String jdbcClassName = "com.ibm.db2.jcc.DB2Driver";
+        String url = "jdbc:db2://localhost:50001/testlib";
+        String user = "User";
+        String password = "ting970926";
+        Connection conn = null;
+        int counter = 0;
+        //Insert Data into Table
+        try {
+            Class.forName(jdbcClassName);
+            conn = DriverManager.getConnection(url,user,password);
+            System.out.println("Creating statement...");
+            Statement st = conn.createStatement();
+            // Extract records in ascending order by first name.
+            System.out.println("Fetching records in ascending order...");
+            String sql = ("SELECT MT_ID, " +
+                    "MT_TITLE, " +
+                    "MT_ISBN, " +
+                    "MT_PUBLISHER, " +
+                    "MT_PUBLISH_DATE, " +
+                    "MT_EDITION, " +
+                    "MT_SH_ID, " +
+                    "MT_PRICE, " +
+                    "MT_TYPE " +
+                    "from material");
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                data.add(counter,new Vector<String>(10));
+                Vector<String> rowVector = data.get(counter);
+                rowVector.add(rs.getString(1));
+                rowVector.add(rs.getString(2));
+                rowVector.add(rs.getString(3));
+                rowVector.add(rs.getString(4));
+                rowVector.add(rs.getString(5));
+                rowVector.add(rs.getString(6));
+                rowVector.add(rs.getString(7));
+                rowVector.add(rs.getString(8));
+                rowVector.add(rs.getString(9));
+                counter++;
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (conn != null) {
+                System.out.println("Connection success!");
+            }
+            try{
+                conn.close();
+            }catch (SQLException w){}
+
+        }
+
+        Vector<String> columnNames = new Vector<String>(10);
+        columnNames.add("ID");
+        columnNames.add("Title");
+        columnNames.add("ISBN");
+        columnNames.add("Publisher");
+        columnNames.add("Publish Date");
+        columnNames.add("Edition");
+        columnNames.add("Shelf");
+        columnNames.add("Price");
+        columnNames.add("Type");
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+
+        materialTable.setModel(model);
+        TableColumn column = materialTable.getColumnModel().getColumn(1);
+        column.setPreferredWidth(500);
+
+        backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    CardLayout cardLayout = (CardLayout) panel.getLayout();
+                    cardLayout.show(panel, "main");
+                }
+        });
+        addMaterialButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        addMaterialButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) panel.getLayout();
+                cardLayout.show(panel, "add material");
+            }
+        });
+        removeMaterialButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) panel.getLayout();
+                cardLayout.show(panel, "remove material");
+            }
+        });
+        editMaterialButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) panel.getLayout();
+                cardLayout.show(panel, "edit material");
+            }
+        });
+
+    }
+
+    public Vector<Vector<String>> createDataVector(){
+        //Create Data vector
+        //Import from Database
+        //I
+        Vector<Vector<String>> data = new Vector<Vector<String>>(10);
+        String input;
+        String jdbcClassName = "com.ibm.db2.jcc.DB2Driver";
+        String url = "jdbc:db2:testlib";
+        Connection conn = null;
+        int counter = 0;
+
+        try {
+            Class.forName(jdbcClassName);
+            conn = DriverManager.getConnection(url);
+            System.out.println("Creating statement...");
+            Statement st = conn.createStatement();
+            // Extract records in ascending order by first name.
+            System.out.println("Fetching records in ascending order...");
+            String sql = ("SELECT MT_ID, " +
+                    "MT_TITLE, " +
+                    "MT_ISBN, " +
+                    "MT_PUBLISHER, " +
+                    "MT_PUBLISH_DATE, " +
+                    "MT_EDITION, " +
+                    "MT_SH_ID, " +
+                    "MT_PRICE, " +
+                    "MT_TYPE " +
+                    "from material");
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                data.add(counter,new Vector<String>(10));
+                Vector<String> rowVector = data.get(counter);
+                rowVector.add(rs.getString(1));
+                rowVector.add(rs.getString(2));
+                rowVector.add(rs.getString(3));
+                rowVector.add(rs.getString(4));
+                rowVector.add(rs.getString(5));
+                rowVector.add(rs.getString(6));
+                rowVector.add(rs.getString(7));
+                rowVector.add(rs.getString(8));
+                rowVector.add(rs.getString(9));
+                counter++;
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (conn != null) {
+                System.out.println("Connection success!");
+            }
+        }
+
+
+
+        return data;
+    }
+
+}
